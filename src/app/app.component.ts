196eb21946503974fb5c98889764c06722d48693
crisleo94 <cristhian.reinoso@live.com>
2019-04-06 13:09:42 -0500

    estilos login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,7 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
+      this.statusBar.backgroundColorByHexString("transparent");
       this.splashScreen.hide();
     });
   }
