3323b351b60e56cdd8540af6cca4a00b3faef689
crisleo94 <cristhian.reinoso@live.com>
2019-04-06 20:19:41 -0500

    footer del login transparente, creación de admin, creacion de admin-create-championship con el paso uno, organización de las páginas

--- src/app/pages/admin-create-csp-step1/admin-create-csp-step1.page.ts
+++ src/app/pages/admin-create-csp-step1/admin-create-csp-step1.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-admin-create-csp-step1',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminCreateCSPStep1Page implements OnInit {
 
-  constructor() { }
+  constructor(public alertCtrl: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Número de grupos',
+      inputs: [
+        {
+          name: 'numero_grupos',
+          type: 'number'
+        }
+      ],
+      buttons: ['Cancelar', 'OK']
+    });
+
+    await alert.present();
   }
 
 }
