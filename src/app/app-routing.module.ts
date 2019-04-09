e5b7cc65d9c85c6355bbe48971073a49c6a687aa
crisleo94 <cristhian.reinoso@live.com>
2019-04-08 21:32:26 -0500

    continuación de las vistas para la creación del torneo, creando vista adicional para la selección de equipos una vez creado el grupo, se estiliza por completo el create-championship, varios estilos más a las páginas admin ya creadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'user', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
   { path: 'user', loadChildren: './pages/user/user.module#UserPageModule' },
   { path: 'admin', loadChildren: './pages/admin/admin.module#AdminPageModule' },
