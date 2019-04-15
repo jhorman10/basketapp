3d6b64096031cf3210d085aa44101a74137cd4d0
crisleo94 <cristhian.reinoso@live.com>
2019-04-15 13:20:17 -0500

    se organizan estilos de las paginas, en sus SASS locales, se reordena navegación, se modifican algunas vistas del flujo de la creación del torneo

--- src/app/pages/admin-asign-locate/admin-asign-locate.page.ts
+++ src/app/pages/admin-asign-locate/admin-asign-locate.page.ts
@@ -1,3 +1,4 @@
+import { AlertController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminAsignLocatePage implements OnInit {
 
-  constructor() { }
+  constructor(public alertCtrl: AlertController) { }
+
+
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Añadir Fecha y Lugar',
+      inputs: [
+        {
+          name: 'Fecha',
+          type: 'date'
+        },
+        {
+          name: 'Lugar',
+          placeholder: 'Lugar',
+          type: 'text'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        },
+        {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 
   ngOnInit() {
   }
