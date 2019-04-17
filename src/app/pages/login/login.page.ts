dedfd4ef8af91b4f097e6d170a38755817573c0f
crisleo94 <cristhian.reinoso@live.com>
2019-04-17 06:52:36 -0500

    se crean datos locales que se guardarán con localstorage Ãpara probar el login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
@@ -9,24 +10,35 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class LoginPage implements OnInit {
 
   login: FormGroup;
+  correo: string;
+  password: string;
 
-  constructor() { 
+  constructor(private rutas: Router) {
 
     this.login = new  FormGroup({
       'EmailLogin': new FormControl ('', [Validators.required, Validators.email]),
       'PwLogin': new FormControl ('', [Validators.required, Validators.minLength(8)])
-
     });
 
   }
 
 
-loginInput(){
-  console.log(this.login.value);
+loginInput(correo: string, password: string) {
+  this.correo = correo;
+  this.password = password;
+  if (this.correo === 'aol@aol.com' && this.password === '12345678') {
+    this.rutas.navigateByUrl('/admin');
+    this.guardarStorage();
+  }
   this.login.reset({
-    EmailLogin: "",
-    PwLogin: ""
+    EmailLogin: '',
+    PwLogin: ''
   });
+}
+
+guardarStorage() {
+  localStorage.setItem('correo', this.correo);
+  localStorage.setItem('password', this.password);
 }
 
   ngOnInit() {
